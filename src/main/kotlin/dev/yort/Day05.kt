7f07cd5a874e9bdf126526bc8fb4b5810c668347
troy <troy@unverdruss.net>
2025-12-05 22:26:36 -0800

    day 5, part 2

--- src/main/kotlin/dev/yort/Day05.kt
+++ src/main/kotlin/dev/yort/Day05.kt
@@ -15,11 +15,26 @@ object Day05 {
 
     fun part2() {
         val rawInputs = readRawInputs()
-        val ranges = processInputRanges(rawInputs)
+        val rangesSortedByFirst = processInputRanges(rawInputs).sortedBy { it.first }
 
-        val naiveNums = ranges.sumOf { it.last - it.first + 1 }
-        println(naiveNums)
+        val processedRanges = mutableListOf<LongRange>()
 
+        var currRange = rangesSortedByFirst.first()
+        (1..rangesSortedByFirst.lastIndex).forEach { index ->
+            val range = rangesSortedByFirst[index]
+            if (currRange.contains(range.first) && currRange.contains(range.last)) {
+                // Nothing to do here because it's already fully enclosed
+            } else if (currRange.contains(range.first)) {
+                currRange.last = range.last
+            } else {
+                processedRanges.add(currRange.toLongRange())
+                currRange = range
+            }
+        }
+        processedRanges.add(currRange.toLongRange())
+
+        val countOfFreshIngredients = processedRanges.sumOf { it.last - it.first + 1 }
+        println("Day 5, Part 2: There are [${countOfFreshIngredients}] possible fresh ingredients")
     }
 
     private fun readRawInputs(): List<Pair<String, String?>> {
@@ -37,9 +52,14 @@ object Day05 {
         return rawInputs
     }
 
-    private fun processInputRanges(rawInputs: List<Pair<String, String?>>): List<LongRange> =
-        rawInputs.filter { it.second != null }.map { LongRange(it.first.toLong(), it.second!!.toLong()) }
+    private fun processInputRanges(rawInputs: List<Pair<String, String?>>): List<MutableRange> =
+        rawInputs.filter { it.second != null }.map { MutableRange(it.first.toLong(), it.second!!.toLong()) }
 
     private fun processInputIngredients(rawInputs: List<Pair<String, String?>>): List<Long> =
         rawInputs.filter { it.second == null }.map { it.first.toLong() }
+
+    private class MutableRange(var first: Long, var last: Long) {
+        fun contains(v: Long) = v in first..last
+        fun toLongRange() = first..last
+    }
 }
